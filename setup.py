a2383b1aee54743fd00410d64ff0220d8d1b335d
Sammy Barasa <barsamms@gmail.com>
2020-07-03 12:43:21 +0300

    Updated file

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Setup script for realpython-reader"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="realpython-reader",
+    version="1.0.0",
+    description="Read the latest Real Python tutorials",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Latest release tag",
+    author_email="barsamms@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=[
+        "PyGithub "
+    ],
+    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+)

edef4f1a0c0afc00705a16004ab4742cd6c0d7b7
Sammy Barasa <barsamms@gmail.com>
2020-07-03 13:32:59 +0300

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,34 +1,20 @@
-"""Setup script for realpython-reader"""
-
-import os.path
 from setuptools import setup
 
-# The directory containing this file
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-# The text of the README file
-with open(os.path.join(HERE, "README.md")) as fid:
-    README = fid.read()
-
-# This call to setup() does all the work
 setup(
-    name="realpython-reader",
-    version="1.0.0",
-    description="Read the latest release tag",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/realpython/reader",
-    author="Latest release tag",
-    author_email="barsamms@gmail.com",
-    license="MIT",
+    name='py-github',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/masgeek/py-github.git',
+    license='MIT',
+    author='Sammy Barasa',
+    author_email='barsamms@gmail.com',
+    description='Python github api utilities',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
     ],
-    packages=["reader"],
-    include_package_data=True,
+    python_requires='>=3',
     install_requires=[
         "PyGithub", "python-dotenv[cli]"
     ],
-    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
 )
